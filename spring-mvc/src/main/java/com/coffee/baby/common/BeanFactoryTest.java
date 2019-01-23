351b3aaef14285ef9129947fb048dd81c19fdf75
smile <smile.szx@outlook.com>
2019-01-24 00:22:09 +0800

    analysis spring doGetBean method source code

---
+++ spring-mvc/src/main/java/com/coffee/baby/common/BeanFactoryTest.java
@@ -0,0 +1,28 @@
+package com.coffee.baby.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * @Description TODO
+ * @Author Thinkpad
+ * @Date 2019/1/23 23:33
+ **/
+@Slf4j
+public class BeanFactoryTest {
+
+    public static void main(String[] args) {
+
+        //https://blog.csdn.net/qq_41541619/article/details/79592225  XmlBeanFactory 3.1 废弃解决方案
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("beanFactoryTest.xml");
+
+        MyTestBean bean = (MyTestBean) beanFactory.getBean("myTestBean");
+        System.out.println(bean.getTestStr());
+    }
+}
---
+++ spring-mvc/src/main/java/com/coffee/baby/common/MyTestBean.java
@@ -0,0 +1,18 @@
+package com.coffee.baby.common;
+
+/**
+ * @Description TODO
+ * @Author Thinkpad
+ * @Date 2019/1/23 23:30
+ **/
+public class MyTestBean {
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
